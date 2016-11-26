15e41faabfab799c6d53ed06f260c0bcea4dc909
Russell Cousineau <rcousineau@comscore.com>
2016-11-26 15:14:29 -0800

    imports&exports: foomodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { FooModule } from './foo/foo.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    FooModule
   ],
   providers: [],
   bootstrap: [AppComponent]
